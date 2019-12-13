6e479027f9ff4e2867204c8b1dab1884acffffc6
marcos <marcosvto1@gmail.com>
2019-12-13 11:28:14 -0300

    enviando mensagem

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   root to: "home#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :contacts, only: %i(index new create destroy)
-  resources :chats, only: %i(index show)
+  resources :chats, only: %i(index show) do
+    resources :messages, only: :create, default: {format: :js}
+  end
 end
